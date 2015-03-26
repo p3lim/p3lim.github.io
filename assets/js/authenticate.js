32019798bd5d67486612784b147dafc5cf143517
Adrian L Lange <mail@p3lim.net>
2015-03-26 04:08:37 +0100

    Preliminary work on getting comments ready
    Basic GitHub OAuth authentication

---
+++ assets/js/authenticate.js
@@ -0,0 +1,76 @@
+var authURL = 'https://github.com/login/oauth/',
+	clientID = '503b0baa4664a76b6718';
+
+var onAuthClick = function(event){
+	event.preventDefault();
+
+	var chars = '0123456789abcdef';
+	var randomString = '';
+
+	for(var index = 0; index < 7; index++){
+		var selection = Math.floor(Math.random() * chars.length);
+		randomString += chars.substring(selection, selection + 1);
+	}
+
+	localStorage.setItem('state', randomString);
+	location.href = authURL.format(clientID, 'public_repo', randomString, encodeURIComponent(location.href));
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	var authAnchor = this.querySelector('.comments .post .authenticate');
+	var avatar = this.querySelector('.comments .post > img');
+	if(localStorage.getItem('token')){
+		avatar.title = localStorage.getItem('username');
+		avatar.src = localStorage.getItem('avatar');
+
+		return;
+	}
+
+	var storedCode = localStorage.getItem('code');
+	if(code){
+		localStorage.removeItem('code');
+		location.hash = 'comments';
+
+		post(authURL + 'access_token', JSON.stringify({
+			client_id: clientID,
+			client_secret: '',
+			code: storedCode
+		}), function(req, error){
+			if(error)
+				return;
+
+			var token = req.response.split('access_token=')[1].split('&')[0];
+			localStorage.setItem('token', token);
+
+			get('https://api.github.com/user', function(req, error){
+				if(error)
+					return;
+
+				var data = JSON.parse(req.response);
+				localStorage.setItem('username', data.login);
+				localStorage.setItem('avatar', data.avatar_url);
+
+				avatar.title = data.login;
+				avatar.src = data.avatar_url;
+
+				authAnchor.style.display = 'none';
+			}, {Authorization: 'token ' + token});
+		});
+
+		return;
+	}
+
+	var match, code, state = location.search.match(/^\?code=(.+)&state=(.+)/);
+	if(match){
+		var savedState = localStorage.getItem('state');
+		if(savedState && savedState === state){
+			localStorage.removeItem('state');
+			localStorage.setItem('code', code);
+			location.href = location.href.split('?')[0];
+		}
+	}
+
+	authURL += 'authorize?client_id={0}&scope={1}&state={2}&redirect_uri={3}';
+	authAnchor.style.display = 'block'
+	authAnchor.children[0].addEventListener('click', onAuthClick);
+});
--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -11,3 +11,33 @@ function fixAnchors(){
 }
 
 document.addEventListener('DOMContentLoaded', fixAnchors);
+
+var xhr = function(method, path, callback, headers, data){
+	var req = new XMLHttpRequest();
+	req.open(method, path, true);
+
+	for(var header in headers)
+		req.setRequestHeader(header, headers[header]);
+
+	req.onreadystatechange = function(){
+		if(this.readyState === 4)
+			callback(this, this.status !== 200);
+	}
+
+	xhr.send(data);
+}
+
+function get(path, callback, headers){
+	xhr('GET', path, callback, headers);
+}
+
+function post(path, data, callback, headers){
+	xhr('POST', path, callback, headers, data);
+}
+
+String.prototype.format = function(){
+	var formatted = this;
+	for(var arg in arguments)
+		formatted = formatted.replace('{' + arg + '}', arguments[arg]);
+	return formatted;
+}
