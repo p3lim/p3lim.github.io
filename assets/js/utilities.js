2305f7f7391a814b896a4b480f847c9cd5f30c27
Adrian L Lange <mail@p3lim.net>
2015-03-26 03:12:15 +0100

    Make sure the anchor actually has an href

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,11 +1,12 @@
 function fixAnchors(){
 	var anchors = document.querySelectorAll('a[href]');
 	for(index in anchors){
-		var anchor = anchors[index],
-			host = anchor.href.match(/\/\/(.+\.\w+)\/?/);
-
-		if(host && host[1] !== location.host)
-			anchor.setAttribute('target', '_blank');
+		var anchor = anchors[index];
+		if(anchor && anchor.href){
+			var host = anchor.href.match(/\/\/(.+\.\w+)\/?/);
+			if(host && host[1] !== location.host)
+				anchor.setAttribute('target', '_blank');
+		}
 	}
 }
 
