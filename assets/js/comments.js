4de4845feb40608fc2755355eabc5b61f7b142e5
Adrian L Lange <mail@p3lim.net>
2018-05-04 21:00:11 +0200

    Update comments script, ES6 yo

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,69 +1,66 @@
+// https://p3lim.net/2018/05/04/github-powered-comments
+
 const article = document.querySelector('article');
 const nwo = article.getAttribute('data-nwo'); // site.github.repository_nwo
 const sha = article.getAttribute('data-sha'); // page.sha (custom front-matter)
 
-let section = document.createElement('section');
-section.id = 'comments';
-section.appendChild(document.createElement('hr'));
-article.appendChild(section);
+let get = url => {
+	// Promise-based XMLHttpRequest
+	return new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest();
+		xhr.onerror = () => reject(xhr.statusText);
+		xhr.onload = () => (xhr.status === 200) ? resolve(xhr.response) : reject(xhr.statusText);
+		xhr.open('GET', url);
+		xhr.send();
+	});
+};
 
-const TEMPLATE = `
-<div class='avatar'>
-	<a href='{user_url}'>
-		<img alt='@{user_name}' src='{avatar_url}'>
-	</a>
-</div>
-<div class='comment'>
-	<div class='header'>
-		<h3>
-			<strong>
-				<a href='{user_url}'>{user_name}</a>
-			</strong>
-			commented
-			<a class='timestamp' href='{comment_url}'>
-				<relative-time datetime='{comment_date}' title='{time_accurate}'>{time_relative}</relative-time>
-			</a>
-		</h3>
-	</div>
-	<div class='body gfm'>
-		{comment_body}
-	</div>
-</div>
-`
+let template = comment => {
+	// returns HTML markup based on template interpolated with data
+	let time = moment(comment.created_at);
+	return `
+		<div ${comment.author_association === 'OWNER' ? `class='author'` : ''}>
+			<div class='avatar'>
+				<a href='${comment.user.html_url}'>
+					<img alt='@${comment.user.login}' src='${comment.user.avatar_url}'>
+				</a>
+			</div>
+			<div class='comment'>
+				<div class='header'>
+					<h3>
+						<strong>
+							<a href='${comment.user.html_url}'>${comment.user.login}</a>
+						</strong>
+						commented
+						<a class='timestamp' href='${comment.html_url}'>
+							<relative-time datetime='${comment.created_at}' title='${time.format('MMM D, YYYY, HH:mm')}'>${time.fromNow()}</relative-time>
+						</a>
+					</h3>
+				</div>
+				<div class='body gfm'>
+					${marked(comment.body, {gfm: true})}
+				</div>
+			</div>
+		</div>
+	`;
+};
 
-function handler(){
-	if(this.status !== 200)
-		return;
+// append new comments section to article
+article.insertAdjacentHTML('beforeend', '<section id="comments"><hr></section>');
 
-	let comments = JSON.parse(this.response);
-	for(let index in comments){
-		let comment = comments[index];
-		let box = document.createElement('div');
-		box.innerHTML = TEMPLATE.replace(/{user_url}/g, comment.user.html_url)
-								.replace(/{user_name}/g, comment.user.login)
-								.replace(/{avatar_url}/g, comment.user.avatar_url)
-								.replace(/{time_accurate}/g, moment(comment.created_at).format('MMM D, YYYY, HH:mm'))
-								.replace(/{time_relative}/g, moment(comment.created_at).fromNow())
-								.replace(/{comment_url}/g, comment.html_url)
-								.replace(/{comment_date}/g, comment.created_at)
-								.replace(/{comment_body}/g, marked(comment.body, {gfm:true}));
+// query for the comments section
+let section = document.querySelector('article #comments');
 
-		if(comment.author_association === 'OWNER')
-			box.classList.add('author');
+// request comments for article from GitHub
+get(`https://api.github.com/repos/${nwo}/commits/${sha}/comments`)
+	.then(data => {
+		// parse comments as HTML and append to comments section
+		section.insertAdjacentHTML('beforeend', JSON.parse(data).map(template).join(''));
 
-		section.appendChild(box);
-	}
-
-	let link = document.createElement('a');
-	link.innerText = 'Leave a comment on GitHub';
-	link.href = `https://github.com/${nwo}/commit/${sha}#comments`;
-	let box = document.createElement('div');
-	box.classList.add('gfm');
-	box.appendChild(link);
-	section.appendChild(box);
-}
-
-let xhr = new XMLHttpRequest();
-xhr.onload = handler;
-xhr.open('GET', 'https://api.github.com/repos/' + nwo + '/commits/' + sha + '/comments');
-xhr.send();
+		// append link to commit to comments section
+		section.insertAdjacentHTML('beforeend', `<div class='gfm'><a href='https://github.com/${nwo}/commit/${sha}#comments'>Leave a comment on GitHub</a></div>`);
+	})
+	.catch(error => {
+		// append error message to comment section
+		section.insertAdjacentHTML('beforeend', '<div class="gfm"><p>Failed to fetch comments :(</p></div>');
+	});
